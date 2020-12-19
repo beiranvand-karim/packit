e049afad7c5c6444df692961446c81c1bff00e4f
beiranvand-karim <beiranvand.karim@gmail.com>
2020-12-19 20:21:06 +0100

    copy to build folder

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
 import { copySync } from "https://deno.land/std@0.81.0/fs/mod.ts";
 
-copySync("./app", "./build");
-
-
-export const sayHello = (name: string) => "Hello " + name
+export const copyToBuildFolder = () => copySync("./app", "./build");
